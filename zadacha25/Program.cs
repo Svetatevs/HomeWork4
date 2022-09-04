bf3ac067e38b923074aeeb3ccfb6fe8e22ec6b2d
Svetatevs <urusel.k@mail.ru>
2022-09-04 22:55:09 +0300

    Степень числа

---
+++ zadacha25/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число А: ");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B: ");
+double B = Convert.ToDouble(Console.ReadLine());
+
+double Pow(double a, double b)
+{
+    double result = a * b;
+    return result;
+}
+double x = Math.Pow(A, B);
+Console.WriteLine($"Число А в степени В равно: {x}");
+
